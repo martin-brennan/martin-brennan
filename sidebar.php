77f5d196772805953215ecc0aad3d116e9ff444f
Martin Brennan <mjrbrennan@gmail.com>
2012-07-28 18:01:39 +1000

    Removed twitter

--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,6 @@
 			<li><a class="no_icon" href="http://feeds.feedburner.com/MartinBrennan" rel="alternate" type="application/rss+xml"><img alt="Subscribe via RSS" src="<?php bloginfo('template_directory') ?>/img/rss.png"></img>&nbsp;Subscribe via RSS</a></li>
 			<li><a class="no_icon" href="http://feedburner.google.com/fb/a/mailverify?uri=MartinBrennan&amp;loc=en_US"><img alt="Subscribe via Email" src="<?php bloginfo('template_directory') ?>/img/email.png"></img>&nbsp;Subscribe via Email</a></li>
 			<br/>
-			<a href="https://twitter.com/mjrbrennan" class="twitter-follow-button" data-show-count="false">Follow @mjrbrennan</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		</ul>
 	</div>
 	<h4><strong>Categories</strong></h4>
