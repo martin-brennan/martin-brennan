6c36fe52ab63befd8f3ca3eae291f56327a6e326
Martin Brennan <mjrbrennan@gmail.com>
2013-01-01 18:31:32 +1000

    Changed spacing between loop and header.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <input type="text" value="" name="s" id="s" />
-        <input type="image" src="<?php bloginfo('template_url') ?>/img/search-icon.png" id="searchsubmit" name="searchsubmit" value="Search" style="padding-top: 10px; margin-bottom: -7px" />
+        <input type="image" src="<?php bloginfo('template_url') ?>/img/search-icon.png" id="searchsubmit" name="searchsubmit" value="Search" style="margin-bottom: -7px;" />
     </div>
 </form>
