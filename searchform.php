34292b5d30de5e197f39b9f66ca20edd15981fb3
Martin Brennan <mjrbrennan@gmail.com>
2013-01-16 22:04:53 +1000

    Deleting files.

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <input type="text" value="" name="s" id="s" />
-        <input type="image" src="<?php bloginfo('template_url') ?>/img/search-icon.png" id="searchsubmit" name="searchsubmit" value="Search" style="margin-bottom: -7px;" />
-    </div>
-</form>
