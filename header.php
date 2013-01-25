87af62cdbbbc5ddee09dc8a03625b701945fc70a
Martin Brennan <mjrbrennan@gmail.com>
2013-01-25 13:22:11 +1000

    Made more fixes

--- header.php
+++ header.php
@@ -2,8 +2,9 @@
 <html>
 	<head>
 		<title><?php wp_title(''); ?></title>
-		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		
 		<!-- Import stylesheets -->
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
